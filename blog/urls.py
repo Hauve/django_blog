b6d716e5e8e8455240e53f16130130d3531bd4e0
Hauve <jemazpn@gmail.com>
2023-06-20 01:03:44 +0300

    Updated static and templates location

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.conf.urls import static
 
 from . import views
-
+from django.conf import settings
 
 urlpatterns = [
     path('', views.PostView.as_view()),
